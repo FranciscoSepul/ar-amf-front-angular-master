faa01a86cd9851646f32b6d2914eb970ba2ed63d
fsepulveda <fsepulveda@amf.cl>
2023-09-05 17:12:38 -0300

    Se modifica usuarios

--- src/app/modules/usuarios/usuarios.module.ts
+++ src/app/modules/usuarios/usuarios.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuariosComponent } from '../usuarios/usuarios.component';
+import { ListarComponent } from './listar/listar.component';
+import { CrearComponent } from './crear/crear.component';
+import { EditarComponent } from './editar/editar.component';
 
 
 
 @NgModule({
   declarations: [
-    UsuariosComponent
+    UsuariosComponent,
+    ListarComponent,
+    CrearComponent,
+    EditarComponent
   ],
   imports: [
     CommonModule
