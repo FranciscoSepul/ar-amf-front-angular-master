933e463789784a55b786ca0b6b299285d2463559
Francisco Sepúlveda <fsepulveda@amf.cl>
2023-09-22 16:31:09 -0300

    se deja lista funcional en todos los moduloss pero listando lo susuarios

--- src/app/modules/actividades/listar/listar.component.ts
+++ src/app/modules/actividades/listar/listar.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { SelectItem} from 'primeng/api';
+import { UserServiceService } from '../../../shared/Services/Usuarios/user-service.service';
+import { sortOptions } from '../../../core/common/constants';
+
 
 @Component({
   selector: 'app-listar',
@@ -7,9 +12,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarComponent implements OnInit {
 
-  constructor() { }
+  usuarios: any[];
+  rows = 5;
+  showLoader = false;
+  isDisabled = false;
+  details: any;
+  sortOptions: SelectItem[];
+  @Output() crearUsuario: EventEmitter<any> = new EventEmitter();
+  @Output() detailUser: EventEmitter<any> = new EventEmitter();
+
+  constructor
+    (private UserServiceService: UserServiceService, private router: Router) { }
 
   ngOnInit(): void {
+    this.showLoader = true;
+    this.sortOptions = [...sortOptions];
+    this.getAllUsers();
   }
 
+  Crear() {
+    this.crearUsuario.emit();
+  }
+  async getAllUsers() {
+    (await this.UserServiceService.UserList()).subscribe({
+      next: data => {
+        this.usuarios = data;
+      },
+      error(e) {
+        this.helpers.checkPermission(this.messageService, e);
+      }
+    })
+  }
+  confirmAction(id, name, isDisabled) {
+    this.active(id, !isDisabled);
+  }
+
+  async active(id: number, activation: boolean) {
+    try {
+      let alert = this.usuarios.find(x => x.run_usuario == id)
+      if (alert) {
+        alert.isdelete = 1
+          ; (await this.UserServiceService.UserDisable(alert)).subscribe({
+            next: () => {
+              this.ngOnInit()
+            }
+          });
+      }
+    } catch (error) {
+    }
+  }
+  onSortChange(event) {
+    this.rows = event.value;
+  }
+
+  async seeEditar(id) {
+    this.details = this.usuarios.filter(x => x.run_usuario == id);
+    this.detailUser.emit(this.details[0]);
+  }
 }
+
--- src/app/modules/visitas/visitas.module.ts
+++ src/app/modules/visitas/visitas.module.ts
@@ -6,16 +6,44 @@ import { ListarComponent } from './listar/listar.component';
 import { CrearComponent } from './crear/crear.component';
 import { EditarComponent } from './editar/editar.component';
 
+import { DateComponent} from '../../shared/date/date.component';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressBarModule} from 'primeng/progressbar';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { MessagesModule } from 'primeng/messages';
+import { InputNumberModule } from "primeng/inputnumber";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MessageModule } from 'primeng/message';
 
 @NgModule({
   declarations: [
     ListarComponent,
     CrearComponent,
-    EditarComponent
+    EditarComponent,
+    DateComponent
   ],
   imports: [
     CommonModule,
-    VisitasRoutingModule
+    VisitasRoutingModule,
+    MessageModule,
+    CardModule,
+    TableModule,
+    DropdownModule,
+    ProgressBarModule,
+    ConfirmDialogModule,
+    MessagesModule,
+    InputNumberModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ConfirmDialogModule,
+    InputSwitchModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class VisitasModule { }
