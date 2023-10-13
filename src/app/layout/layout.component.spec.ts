d70bb71e5cbd2d9592e771ac3bf427513ab66e33
Francisco Sepúlveda <fsepulveda@amf.cl>
2023-10-13 18:08:05 -0300

    comit err

--- src/app/layout/layout.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { TestBed, async } from '@angular/core/testing';
-import { LayoutComponent } from './layout.component';
-
-describe('Component: Layout', () => {
-  it('should create an instance', () => {
-    let component = new LayoutComponent();
-    expect(component).toBeTruthy();
-  });
-});
