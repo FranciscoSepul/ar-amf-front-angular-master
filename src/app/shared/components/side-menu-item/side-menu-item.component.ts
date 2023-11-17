b7bbfa7464b5ff728d046272b69599081aaa1e0f
Francisco Sepúlveda <fsepulveda@amf.cl>
2023-11-17 00:05:47 -0300

    merge

--- src/app/shared/components/side-menu-item/side-menu-item.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'side-menu-item',
-  templateUrl: './side-menu-item.component.html',
-  styleUrls: ['./side-menu-item.component.scss']
-})
-export class SideMenuItemComponent {}
