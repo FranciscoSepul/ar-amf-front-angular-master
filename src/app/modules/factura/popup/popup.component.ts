ec3e3fad197790097c58b1b8efc668f24cc877fe
Francisco Sepúlveda <fsepulveda@amf.cl>
2023-11-26 21:14:39 -0300

    se repara error formulario

--- src/app/modules/factura/popup/popup.component.ts
+++ src/app/modules/factura/popup/popup.component.ts
@@ -1,9 +1,7 @@
 import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { CompanyService } from '../../../shared/Services/Company/company.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {FormsModule} from '@angular/forms'  ;
-import {ReactiveFormsModule} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-popup',
@@ -11,72 +9,75 @@ import {ReactiveFormsModule} from '@angular/forms';
   styleUrls: ['./popup.component.scss']
 })
 
-export class PopupComponent{
-  @Input() itemId: number; 
+export class PopupComponent {
+  @Input() itemId: number;
   formb: FormGroup;
 
-  constructor(private fb: FormBuilder,private CompanyService: CompanyService, private modalService: NgbModal) { 
-    this.formb = this.fb.group({
-      idTransaccion: [1, [Validators.required]],
-      Detalle: ['', [Validators.required]],
-      Monto: ['', [Validators.required]]
+  constructor(private CompanyService: CompanyService, private modalService: NgbModal) {
+    //this.formb = this.fb.group({
+    
+    // });
+    this.formb = new FormGroup({
+      idTransaccion: new FormControl(),
+      Detalle: new FormControl(),
+      Monto: new FormControl(),
     });
   }
 
   ngOnInit(): void {
-    console.log(this.itemId); 
-    //this.GetFacturaById();
+    console.log(this.itemId);
+    this.GetFacturaById();
 
   }
-  
+
   async GetFacturaById() {
     (await this.CompanyService.GetFacturaById(this.itemId)).subscribe({
       next: data => {
         this.formb.setValue({
-          idTransaccion :this.itemId,
+          idTransaccion: this.itemId,
           Monto: data.iddetallefactura,
-          Detalle:'Pago Factura Empresa'+ data.nom_empresa,
+          Detalle: 'Pago Factura Empresa' + data.nom_empresa,
           //rut: data.rut + '-' + data.dvRut,
           //fechaCreacion: data.fechaCreacion,
           //fechaFinContrato: new Date(data.fechaFinContrato),
           //correo: data.correo,
-         // direccion: data.direccion,
-         // comuna: data.comuna,
-         // region: data.region,
-         // id_empresa: data.id_empresa,
-         // numeroTelefonico: data.numeroTelefonico,
-         // actividadEconomica: data.actividadEconomica,
+          // direccion: data.direccion,
+          // comuna: data.comuna,
+          // region: data.region,
+          // id_empresa: data.id_empresa,
+          // numeroTelefonico: data.numeroTelefonico,
+          // actividadEconomica: data.actividadEconomica,
           //idPropiedadEmpresa: data.idPropiedadEmpresa,
-         // idTipoDeEmpresa: data.idTipoDeEmpresa,
-         // trabajadoresHombres: data.trabajadoresHombres,
-         // trabajadoresMujeres: data.trabajadoresMujeres,
+          // idTipoDeEmpresa: data.idTipoDeEmpresa,
+          // trabajadoresHombres: data.trabajadoresHombres,
+          // trabajadoresMujeres: data.trabajadoresMujeres,
           //Costos
-        //  costoporaccidente: data.costoporaccidente,
-        //  costoporcharla: data.costoporcharla,
-        //  costoporvisita: data.costoporvisita,
-        //  costobase: data.costobase,
-        //  costoporasesoria: data.costoporasesoria,
-        //  costoporasesoriaespecial: data.costoporasesoriaespecial,
-        //  costoporpersonaextra: data.costoporpersonaextra,
-        //  cantidadDeEmpleadosPorContrato: data.cantidadDeEmpleadosPorContrato,
-        //  startDate: new Date(),
-        //  endDate: new Date(),
-        //  opstartDate: new Date(),
-         // opendDate: new Date(),
-         // costoTotalAccidente: data.totalporaccidente,
-         // costoTotalCharla: data.totalporcharla,
-         // costoTotalVisita:  data.totalporvisita,
-         // costoTotalAsesoria: data.totalporasesoria,
-         //costoTotalAsesoriaEspecial:data.totalporasesoriaespecial,
-         // costoTotalPersonasExtra: data.totalporpersonaextra,
+          //  costoporaccidente: data.costoporaccidente,
+          //  costoporcharla: data.costoporcharla,
+          //  costoporvisita: data.costoporvisita,
+          //  costobase: data.costobase,
+          //  costoporasesoria: data.costoporasesoria,
+          //  costoporasesoriaespecial: data.costoporasesoriaespecial,
+          //  costoporpersonaextra: data.costoporpersonaextra,
+          //  cantidadDeEmpleadosPorContrato: data.cantidadDeEmpleadosPorContrato,
+          //  startDate: new Date(),
+          //  endDate: new Date(),
+          //  opstartDate: new Date(),
+          // opendDate: new Date(),
+          // costoTotalAccidente: data.totalporaccidente,
+          // costoTotalCharla: data.totalporcharla,
+          // costoTotalVisita:  data.totalporvisita,
+          // costoTotalAsesoria: data.totalporasesoria,
+          //costoTotalAsesoriaEspecial:data.totalporasesoriaespecial,
+          // costoTotalPersonasExtra: data.totalporpersonaextra,
 
-         // totalAccidente: data.totalporaccidente,
-         // totalCharla: data.totalporcharla,
-         // totalVisita: data.totalporvisita,
+          // totalAccidente: data.totalporaccidente,
+          // totalCharla: data.totalporcharla,
+          // totalVisita: data.totalporvisita,
           //totalAsesoria: data.totalporasesoria,
           //totalAsesoriaEspecial:data.totalporasesoriaespecial,
           //totalPersonasExtra: data.totalporpersonaextra,
-          
+
         });
       }
     })
@@ -85,10 +86,10 @@ export class PopupComponent{
   confirmAction() {
     this.Facture(this.itemId);
   }
-  close(){
+  close() {
     this.modalService.dismissAll(PopupComponent);
   }
-  async Facture(id:number){
+  async Facture(id: number) {
     (await this.CompanyService.FactureByCompanyById(id)).subscribe({
       next: data => {
         this.modalService.dismissAll(PopupComponent);
